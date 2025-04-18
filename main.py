efc2007bd20b16a425ead5f0d4c3f76f0554a8c6
Kaeley Scruggs <kaeley.scruggs@getweave.com>
2025-04-18 09:31:34 -0600

    add more flask functionality

--- main.py
+++ main.py
@@ -2,23 +2,51 @@ import requests
 import sqlite4
 from datetime import datetime
 from flask import Flask
+from random import randint
 # Flask sql alchemy documentation: https://flask-sqlalchemy.palletsprojects.com/en/stable/quickstart/#create-the-tables
 
 app = Flask(__name__)
 #different routes using the app.route decoration
-@makebold
+the_number = randint(0, 9)
+
+def text_color(function):
+    def wrapper_function():
+        color = randint(0,5)
+        hex = ["FFBF00", "FF7F50", "DE3163", "40E0D0", "6495ED", "CCCCFF"]
+        color_picker = hex[color]
+        return print("<h1>" + function() + "</h1>")
+    return wrapper_function
+
+
+
+
 @app.route("/")
-def hello_world():
-    return"<p>Hello, World!</p>"
+def guess_a_number():
+    return"<h1 style='color:FFBF00'>Guess a number</h1>" + str(the_number)
 
-@app.route("/bye")
-def bye():
-    return "<strong>Bye!</strong>"
 
-#Creating variable paths and converting the path to a specified data type
-@app.route("/username/<name>/<int:number>")
+@app.route("/<name>")
+def green(name):
+    return f"Hello {name + str(12)}"
+
+@text_color
+@app.route("/<int:num>")
+def number(num):
+    if num < 0 or num > 10:
+        return "<h1>That is not a number between 0 and 9</h1>"
+    if num == the_number:
+        return "<h1>You found it!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'"
+    if num > the_number:
+        return "<h1>That number is too high</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'"
+    if num < the_number:
+        return "<h1>That number is too low</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'"
+
+'''@app.route("/username/<name>/<int:number>")
 def greet(name, number):
-    return f"hello there{name}, you are {number} years old!"
+    return f"hello there{name}, you are {number} years old!"'''
 
 if __name__ == "__main__":
     #Run the app in debug mode to auto-reload
